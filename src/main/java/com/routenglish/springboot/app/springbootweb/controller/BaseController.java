afd3f3163d8c67df7e2ad348b2dbc379efdf15ef
Kenan <kenansoyunmez@gmail.com>
2020-07-10 13:50:15 +0200

    feat:add requestmapping

--- src/main/java/com/routenglish/springboot/app/springbootweb/controller/BaseController.java
+++ src/main/java/com/routenglish/springboot/app/springbootweb/controller/BaseController.java
@@ -3,8 +3,10 @@ package com.routenglish.springboot.app.springbootweb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class BaseController {
     @GetMapping(value = "/home")
     public String home(Model model) {
